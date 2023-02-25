fc549bc0d6f2d63b4b8a96025d50442e94746ac9
Danny Carlton <webdesign@dannycarlton.com>
2023-02-25 14:44:35 -0600

    Update sample_local.php

--- public_html/includes/sample_local.php
+++ public_html/includes/sample_local.php
@@ -15,7 +15,9 @@ $dbuser='change to database user name';
 $dbpassword='change to database password';
 $dbname='change to database name';
 
-$_path=str_replace('change to main web accessible folder','',$_SERVER['DOCUMENT_ROOT']);
+$_path=str_replace('change to main web accessible folder, typically public_html','',$_SERVER['DOCUMENT_ROOT']);
+/* In most web installations your site will be located at /home/account_name/public_html/ or /home/account_name/site_name/
+/* The $_path variable allows the script to know where it is, and also locate the smarty folder in relation to the web folder. */
 
 $_smarty='change to smarty folder';
 
